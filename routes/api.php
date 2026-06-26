74a06fa42bf28469ac836868c6178ceb5516e779
ikkidzakki <ikkidzakki@gmail.com>
2026-06-26 17:24:57 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ScheduleController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('apikey')->group(function () {
 
-    Route::get('/v1/schedules', [ScheduleController::class, 'index']);
-    Route::get('/v1/schedules/{id}', [ScheduleController::class, 'show']);
-    Route::post('/v1/schedules', [ScheduleController::class, 'store']);
+    Route::prefix('v1')->group(function () {
+
+        Route::get('/schedules', [ScheduleController::class, 'index']);
+
+        Route::get('/schedules/{id}', [ScheduleController::class, 'show']);
+
+        Route::post('/schedules', [ScheduleController::class, 'store']);
+
+    });
 
 });
